eeb55741720ffe5580caa03a54f57345825eee79
Michael Bonani <michael.bonani@mobsya.org>
2016-01-13 12:53:32 +0100

    Update build.sh
    add dashes options

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ mkdir -p "$WORKSPACE/build/dashel"
 cd "$WORKSPACE/build/dashel"
 cmake\
  -D "CMAKE_BUILD_TYPE=Release"\
- -D "BUILD_SHARED_LIBS=OFF"\
+ -D "BUILD_SHARED_LIBS=ON"\
+ -D "CMAKE_OSX_ARCHITECTURES=i386;x86_64"
+ -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.6"
+ -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
  "$WORKSPACE/source/dashel"
 make
 
