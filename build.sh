159280e6917bf3b01a1ff5d24e70bc22ee1f1602
Michael Bonani <michael.bonani@mobsya.org>
2016-01-13 15:47:26 +0100

    Update build.sh
    testing compilation

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cmake\
  -D "BUILD_SHARED_LIBS=ON"\
  -D "CMAKE_OSX_ARCHITECTURES=i386;x86_64"\
  -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.6"\
- -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"\
+ -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk"\
  "$WORKSPACE/source/dashel"
 make
 
@@ -16,9 +16,9 @@ mkdir -p "$WORKSPACE/build/enki"
 cd "$WORKSPACE/build/enki"
 cmake\
  -D "CMAKE_BUILD_TYPE=Release"\
- -D "CMAKE_OSX_ARCHITECTURES=i386;x86_64"\
+ -D "CMAKE_OSX_ARCHITECTURES=x86_64"\
  -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.6"\
- -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"\
+ -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk"\
  "$WORKSPACE/source/enki"
 make
 
@@ -28,13 +28,13 @@ cmake\
  -D "CMAKE_BUILD_TYPE=Release"\
  -D "CMAKE_OSX_ARCHITECTURES=i386;x86_64"\
  -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.6"\
- -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"\
+ -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk"\
  -D "dashel_DIR=$WORKSPACE\build\dashel"\
  -D "DASHEL_INCLUDE_DIR=$WORKSPACE/source/dashel"\
  -D "DASHEL_LIBRARY=$WORKSPACE/build/dashel/libdashel.dylib"\
  -D "ENKI_INCLUDE_DIR=$WORKSPACE/source/enki"\
  -D "ENKI_LIBRARY=$WORKSPACE/build/enki/libenki.a"\
- -D "ENKI_VIEWVER_LIBRARY=$WORKSPACE/build/enki/libenkiviewer.a"\
+ -D "ENKI_VIEWVER_LIBRARY=$WORKSPACE/build/enki/viewer/libenkiviewer.a"\
  "$WORKSPACE/source/aseba"
 make
 
