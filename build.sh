39d4e8ce0fbb67fcb83232c7285c21769ab74785
davidjsherman <david.sherman@inria.fr>
2016-05-03 23:53:20 +0200

    add parameter for MacOSX SDK

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo QWT PATH is ${your_qwt_path:='/usr/local/qwt-6.1.2'}
+echo MACOSX_SDK is ${MACOSX_SDK:='MacOSX10.11.sdk'}
 set -eu
 
 [ -d "$WORKSPACE/source" -o -L "$WORKSPACE/source" ] || ln -s . "$WORKSPACE/source"
@@ -11,7 +12,7 @@ cmake\
  -D "BUILD_SHARED_LIBS=ON"\
  -D "CMAKE_OSX_ARCHITECTURES=i386;x86_64"\
  -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.6"\
- -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk"\
+ -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/$MACOSX_SDK"\
  "$WORKSPACE/source/dashel"
 make
 
@@ -21,7 +22,7 @@ cmake\
  -D "CMAKE_BUILD_TYPE=Release"\
  -D "CMAKE_OSX_ARCHITECTURES=x86_64"\
  -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.6"\
- -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk"\
+ -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/$MACOSX_SDK"\
  "$WORKSPACE/source/enki"
 make
 
@@ -31,7 +32,7 @@ cmake\
  -D "CMAKE_BUILD_TYPE=Release"\
  -D "CMAKE_OSX_ARCHITECTURES=x86_64"\
  -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.6"\
- -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk"\
+ -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/$MACOSX_SDK"\
  -D "dashel_DIR=$WORKSPACE/build/dashel"\
  -D "DASHEL_INCLUDE_DIR=$WORKSPACE/source/dashel"\
  -D "DASHEL_LIBRARY=$WORKSPACE/build/dashel/libdashel.dylib"\
