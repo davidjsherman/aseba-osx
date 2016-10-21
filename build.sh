b8db83d437ffa80158053fba7893dec42afd3920
David James Sherman <david.sherman@inria.fr>
2016-10-21 15:59:49 +0200

    static dashel

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mkdir -p "$WORKSPACE/build/dashel"
 cd "$WORKSPACE/build/dashel"
 cmake\
  -D "CMAKE_BUILD_TYPE=Release"\
- -D "BUILD_SHARED_LIBS=ON"\
+ -D "BUILD_SHARED_LIBS=OFF"\
  -D "CMAKE_OSX_ARCHITECTURES=i386;x86_64"\
  -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.9"\
  -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/$MACOSX_SDK"\
@@ -20,6 +20,7 @@ mkdir -p "$WORKSPACE/build/enki"
 cd "$WORKSPACE/build/enki"
 cmake\
  -D "CMAKE_BUILD_TYPE=Release"\
+ -D "BUILD_SHARED_LIBS=OFF"\
  -D "CMAKE_OSX_ARCHITECTURES=x86_64"\
  -D "CMAKE_OSX_DEPLOYMENT_TARGET=10.9"\
  -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/$MACOSX_SDK"\
@@ -35,7 +36,7 @@ cmake\
  -D "CMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/$MACOSX_SDK"\
  -D "dashel_DIR=$WORKSPACE/build/dashel"\
  -D "DASHEL_INCLUDE_DIR=$WORKSPACE/source/dashel"\
- -D "DASHEL_LIBRARY=$WORKSPACE/build/dashel/libdashel.dylib"\
+ -D "DASHEL_LIBRARY=$WORKSPACE/build/dashel/libdashel.a"\
  -D "ENKI_INCLUDE_DIR=$WORKSPACE/source/enki"\
  -D "ENKI_LIBRARY=$WORKSPACE/build/enki/enki/libenki.a"\
  -D "ENKI_VIEWER_LIBRARY=$WORKSPACE/build/enki/viewer/libenkiviewer.a"\
